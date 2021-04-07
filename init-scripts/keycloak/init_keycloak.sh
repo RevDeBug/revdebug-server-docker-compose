f6a07aaea52cfa0f66b97a96a292192c1ad6c15f
Michał Kostrzewski <michal@revdebug.com>
2021-04-07 21:19:25 +0200

    Keycloak initialization (db and master realm stub).

---
+++ init-scripts/keycloak/init_keycloak.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+# kcadm resides here:
+cd bin
---
+++ init-scripts/postgres/init_keycloak_db.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE keycloak;
+    GRANT ALL PRIVILEGES ON DATABASE keycloak TO $POSTGRES_USER;
+EOSQL
